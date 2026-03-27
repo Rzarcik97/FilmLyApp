ba84786b7b47955d0393547e97238ca8581a1c13
TetiankaSh <shh.tetiana@gmail.com>
2026-03-27 11:13:07 +0200

    local commit for merge

---
+++ frontend/src/components/OverviewPage/OverviewPage.tsx
@@ -0,0 +1,17 @@
+import type { Actor, Movie } from '../../types';
+import { mockActors, mockMovies } from '../../utils/mockData';
+import { ScrollActors } from '../MainPage/ScrollActors';
+import { ScrollSection } from '../MainPage/ScrollSection';
+import { MainOverview } from './MainOverview';
+import { MoreDetails } from './MoreDetails';
+
+export const OverviewPage = () => {
+  return (
+    <div className="bg-light-background">
+      <MainOverview />
+      <MoreDetails />
+      <ScrollActors title="Cast" items={mockActors}/>
+      <ScrollSection title="Yoy may also like" items={mockMovies} />
+    </div>
+  )
+}
