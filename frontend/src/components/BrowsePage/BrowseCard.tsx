0a5bbbcc4fd5408e98dbd6daa67832ad5b23ed0a
TetiankaSh <shh.tetiana@gmail.com>
2026-04-30 18:38:03 +0300

    some clean up

--- frontend/src/components/BrowsePage/BrowseCard.tsx
+++ frontend/src/components/BrowsePage/BrowseCard.tsx
@@ -1,13 +1,17 @@
 import { Link, useLocation } from 'react-router-dom';
-import empty_img from '../../../public/icons/empty-img.png';
-import type { Movie } from '../../types';
+import empty_img from '/icons/empty-img.png';
 
 interface BrowseCardProps {
   genre: string;
+  imagePath?: string | undefined;
 }
 
-export const BrowseCard = ({ genre }: BrowseCardProps) => {
+export const BrowseCard = ({ genre, imagePath }: BrowseCardProps) => {
   const location = useLocation();
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+
+  const hasImage = imagePath && imagePath !== 'string';
+  const imageUrl = imagePath ? `${baseUrl}${imagePath}` : empty_img;
 
   return (
     <div className="w-[204px] h-[254px] flex flex-col shrink-0 rounded-[16px] overflow-hidden
@@ -20,16 +24,29 @@ export const BrowseCard = ({ genre }: BrowseCardProps) => {
       <Link
         to={`/browse/${genre.toLowerCase().replace(/ /g, '-')}`}
         state={{ from: location.pathname }}
+        className="flex flex-col h-full w-full"
       >
-        <div className="bg-gray-90 h-[208px] flex-1 flex justify-center items-center">
+        <div className="bg-gray-90 flex-1 flex justify-center items-center overflow-hidden">
           <img
-            src={empty_img}
-            alt="Item Main Image"
-            className="w-27 h-27"
+            src={imageUrl}
+            alt={genre}
+            className={`transition-all duration-500 
+              ${hasImage
+                ? 'w-full h-full object-cover'
+                : 'w-27 h-27 object-contain'
+              }`}
+            onError={(e) => {
+              const target = e.target as HTMLImageElement;
+              target.src = empty_img;
+              target.className = 'w-27 h-27 object-contain';
+            }}
           />
         </div>
-        <div className="px-2 bg-gray-100 h-12 shrink-0 flex items-center">
-            <p className="text-gray-0 text-[16px] leading-[1.5] font-bold font-nunito">{genre}</p>     
+
+        <div className="px-2 bg-gray-100 h-12 shrink-0 flex items-center z-10">
+          <p className="text-gray-0 text-[16px] leading-[1.5] font-bold font-nunito truncate">
+            {genre}
+          </p>
         </div>
       </Link>
     </div>
