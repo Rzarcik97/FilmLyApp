c20861ccead0a7d25a5b56f28f3121f99e7ece2d
Karol Gajda <karolgaj97@interia.pl>
2026-05-20 14:29:02 +0200

    Fix TMDB RestClient gzip response handling

--- backend/src/main/java/filmly/config/TmdbClientConfig.java
+++ backend/src/main/java/filmly/config/TmdbClientConfig.java
@@ -1,5 +1,6 @@
 package filmly.config;
 
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,13 @@ public class TmdbClientConfig {
 
     @Bean
     public RestClient tmdbRestClient() {
+
+        HttpClients.custom()
+                .disableContentCompression()
+                .build();
+
         String baseUrl = "https://api.themoviedb.org/3";
+
         return RestClient.builder()
                 .baseUrl(baseUrl)
                 .defaultHeader("Authorization", "Bearer " + token)
